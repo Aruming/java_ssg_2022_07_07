3b2a4f3ee3856bdbf0c7c5105124b011216e2257
Aruming <0430kar@naver.com>
2022-07-07 15:33:38 +0900

    등록 명령 추가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -15,6 +15,15 @@ public class App {
             String cmd = sc.nextLine().trim();
 
             switch (cmd){
+                case "등록":
+                    System.out.printf("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.printf("작가 : ");
+                    String author = sc.nextLine().trim();
+                    System.out.println(num+"번 명언이 등록되었습니다.");
+                    num++;
+                    break;
+
                 case "종료":
                     break outer;
             }
