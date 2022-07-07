7ad78222c17515a6da1651d0dab2377229ba87ce
Aruming <0430kar@naver.com>
2022-07-07 15:31:42 +0900

    App 클래스 추가 및 명령 프롬프트 구현

---
+++ src/main/java/com/ll/exam/App.java
@@ -0,0 +1,25 @@
+package com.ll.exam;
+
+import java.util.Scanner;
+
+public class App {
+    public void run(){
+        System.out.println("== 명언 SSG ==");
+
+        Scanner sc = new Scanner(System.in);
+        int num=1;
+
+        outer:
+        while (true){
+            System.out.print("명령) ");
+            String cmd = sc.nextLine().trim();
+
+            switch (cmd){
+                case "종료":
+                    break outer;
+            }
+        }
+
+        sc.close();
+    }
+}
