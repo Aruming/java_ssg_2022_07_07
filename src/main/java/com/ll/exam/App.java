418d441b26d09fbdd43a5d09ee12c871ed67a13e
Aruming <0430kar@naver.com>
2022-07-08 14:56:31 +0900

    입력받은 명언을 리스트에 저장, 목록 기능 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,6 +1,10 @@
 package com.ll.exam;
 
 import java.util.Scanner;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class App {
     public void run(){
@@ -8,7 +12,8 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
         //가장 마지막 명언글의 번호
-        int num=0;
+        List<WiseSaying> wiseSayings = new ArrayList<>();
+        int wiseSayingLastId = 0;
 
         outer:
         while (true){
@@ -21,12 +26,21 @@ public class App {
                     String content = sc.nextLine().trim();
                     System.out.printf("작가 : ");
                     String author = sc.nextLine().trim();
-                    int id = ++num; //명언 글번호 증가
+                    int id = ++wiseSayingLastId; //명언 글번호 증가
 
-                    WiseSaying ws = new WiseSaying(id, content, author);
-                    System.out.println(ws);
+                    WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                    wiseSayings.add(wiseSaying);
 
                     System.out.println(id+"번 명언이 등록되었습니다.");
+                    break;
+
+                case "목록":
+                    System.out.println("번호 / 명언 / 작가");
+                    System.out.println("------------------");
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying_ = wiseSayings.get(i);
+                        System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.content, wiseSaying_.author);
+                    }
                     break;
 
                 case "종료":
