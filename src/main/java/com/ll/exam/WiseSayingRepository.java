4bc723d84c6e5a57e01acbdefe9fd16fbb497345
Aruming <0430kar@naver.com>
2022-07-12 17:15:44 +0900

    컨트롤러에서 본인에게 맞는 역할만 수행하고 나머지 부분을 하위 기관에 위임

--- src/main/java/com/ll/exam/WiseSayingRepository.java
+++ src/main/java/com/ll/exam/WiseSayingRepository.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class WiseSayingRepository {
 
-    public int wiseSayingLastId;
-    public List<WiseSaying> wiseSayings;
+    private int wiseSayingLastId;
+    private List<WiseSaying> wiseSayings;
     WiseSayingRepository(){
         wiseSayings = new ArrayList<>();
         wiseSayingLastId = 0;
@@ -19,5 +19,33 @@ public class WiseSayingRepository {
             }
         }
         return null;
+        //하나 찾기
+    }
+
+    public List<WiseSaying> findAll() {
+        return wiseSayings;
+        //전부 찾기
+    }
+
+    public WiseSaying write(String content, String author) {
+        int id = ++wiseSayingLastId;
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+        wiseSayings.add(wiseSaying);
+
+        return wiseSaying;
+        //파일 저장
+    }
+
+    public void remove(int paramId) {
+        WiseSaying foundWiseSaying = findById(paramId);
+        wiseSayings.remove(foundWiseSaying);
+        //파일 삭제
+    }
+
+    public void modify(int paramId, String content, String author) {
+        WiseSaying foundWiseSaying = findById(paramId);
+        foundWiseSaying.content = content;
+        foundWiseSaying.author = author;
+        //파일수정
     }
 }
